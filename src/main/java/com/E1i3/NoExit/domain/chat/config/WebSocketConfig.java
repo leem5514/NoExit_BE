68bb9dcb3a39ed6b6e379410530d8e22202ed7cc
LEE MYEONGGYU <116091798+leem5514@users.noreply.github.com>
2026-03-19 02:53:04 +0900

    fix

--- src/main/java/com/E1i3/NoExit/domain/chat/config/WebSocketConfig.java
+++ src/main/java/com/E1i3/NoExit/domain/chat/config/WebSocketConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     private final AuthChannelInterceptor authChannelInterceptor;
 
     public WebSocketConfig(AuthChannelInterceptor authChannelInterceptor) {
@@ -18,23 +21,30 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        // 메시지 브로커 구성
-        config.enableSimpleBroker("/topic", "/queue"); // topic :N명, queue : 1:1
+        config.enableSimpleBroker("/topic", "/queue");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .setAllowedOrigins("https://server.noexxit.store")
-                .setAllowedOrigins("https://www.noexxit.store")
+                .setAllowedOrigins(
+                        "https://www.noexxit.store",
+                        "https://server.noexxit.store",
+                    )
                 .withSockJS();
-//                .setClientLibraryUrl("https://cdn.jsdelivr.net/sockjs/1.4.0/sockjs.min.js"); // SockJS 라이브러리 경로 설정
     }
+
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(16);
+        executor.setQueueCapacity(100);
+        executor.initialize();
+
         registration.interceptors(authChannelInterceptor)
-                .taskExecutor(new ThreadPoolTaskExecutor());
+                .taskExecutor(executor);
     }
 
 }
