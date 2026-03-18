34d06b8f6637db31a82550c34dc4c2849f3e75cf
LEE MYEONGGYU <116091798+leem5514@users.noreply.github.com>
2026-03-19 02:19:43 +0900

    fix

--- src/main/java/com/E1i3/NoExit/domain/common/configs/WebConfigs.java
+++ src/main/java/com/E1i3/NoExit/domain/common/configs/WebConfigs.java
@@ -18,8 +18,13 @@ public class WebConfigs implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("https://server.noexxit.store")  // 허용할 클라이언트 도메인
+	    registry.addMapping("/**")
+	            .allowedOrigins(
+	                    "https://www.noexxit.store",
+	                    "https://server.noexxit.store",
+	                    "http://localhost:3000",
+	                    "http://localhost:8082"
+	            )
 				.allowedOrigins("https://www.noexxit.store")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
 				.allowedHeaders("*")
