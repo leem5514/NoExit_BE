70a2d6032b8fb45acac92c42b1e56db9930f5531
LEE MYEONGGYU <116091798+leem5514@users.noreply.github.com>
2026-03-19 02:44:40 +0900

    Fix : 추가 서비스 구문 삭제
    DB 저장
    Redis 발행

--- src/main/java/com/E1i3/NoExit/domain/chat/service/ChatService.java
+++ src/main/java/com/E1i3/NoExit/domain/chat/service/ChatService.java
@@ -1,20 +1,11 @@
 package com.E1i3.NoExit.domain.chat.service;
 
-
 import com.E1i3.NoExit.domain.chat.domain.ChatMessageEntity;
 import com.E1i3.NoExit.domain.chat.domain.ChatRoom;
 import com.E1i3.NoExit.domain.chat.dto.ChatMessage;
 import com.E1i3.NoExit.domain.chat.repository.ChatMessageRepository;
 import com.E1i3.NoExit.domain.chat.repository.ChatRoomRepository;
-import com.E1i3.NoExit.domain.member.domain.Member;
-import com.E1i3.NoExit.domain.member.repository.MemberRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,17 +14,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatService {
 
-    @Qualifier("chatRedisTemplate")
-    private final RedisTemplate<String, Object> redisTemplateChat;
-    @Qualifier("chatRedisMessageListenerContainer")
-    private final RedisMessageListenerContainer redisContainer;
-
     private final RedisMessagePublisher redisMessagePublisher;
-    //    private final RedisStreamService redisStreamService;
     private final ChatMessageRepository chatMessageRepository;
     private final ChatRoomRepository chatRoomRepository;
-    private final MemberRepository memberRepository;
-    private final ObjectMapper objectMapper;
 
     public List<ChatMessageEntity> getMessagesForRoom(Long roomId) {
         ChatRoom chatRoom = chatRoomRepository.findById(roomId)
@@ -41,47 +24,25 @@ public class ChatService {
         return chatMessageRepository.findByChatRoom(chatRoom);
     }
 
-    // 메시지를 처리하고 저장하는 기능
-    public void handleMessage(String roomId, String senderEmail, String senderName, String senderProfileImage, Object content) {
-        //String senderEmail = SecurityContextHolder.getContext().getAuthentication().getName();
-
-
-        ChatRoom chatRoom = chatRoomRepository.findById(Long.parseLong(roomId))
+    public void handleMessage(ChatMessage chatMessage) {
+        ChatRoom chatRoom = chatRoomRepository.findById(Long.parseLong(chatMessage.getRoomId()))
                 .orElseThrow(() -> new IllegalArgumentException("Invalid room ID"));
 
-        // JSON으로 content를 변환
-        String contentAsString;
-        try {
-            contentAsString = objectMapper.writeValueAsString(content);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to serialize content", e);
-        }
-
         ChatMessageEntity chatMessageEntity = ChatMessageEntity.builder()
-                .sender(senderEmail)
-                .senderName(senderName)
-                .senderProfileImage(senderProfileImage)
-                .content(contentAsString)
+                .sender(chatMessage.getSender())
+                .senderName(chatMessage.getSenderName())
+                .senderProfileImage(chatMessage.getSenderProfileImage())
+                .content(chatMessage.getContent())
                 .chatRoom(chatRoom)
-                .timestamp(System.currentTimeMillis())
+                .timestamp(chatMessage.getTimestamp())
                 .build();
 
-        // 메시지 저장
         chatMessageRepository.save(chatMessageEntity);
-        System.out.println("Saved message to database: " + contentAsString);
 
-        // Redis로 메시지 퍼블리시
-        redisMessagePublisher.publish(contentAsString);
-        System.out.println("Published message to Redis: " + contentAsString);
+        redisMessagePublisher.publish(chatMessage);
     }
 
-
-    // 특정 방의 모든 메시지 조회
-    public List<ChatMessageEntity> getMessagesByRoomId(Long roomId) {
-        ChatRoom chatRoom = chatRoomRepository.findById(roomId)
-                .orElseThrow(() -> new IllegalArgumentException("Room not found"));
-        return chatMessageRepository.findByChatRoom(chatRoom);
+    public void publishOnly(ChatMessage chatMessage) {
+        redisMessagePublisher.publish(chatMessage);
     }
 }
-
-
